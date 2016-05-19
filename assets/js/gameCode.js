6837f650e6af8fb2bfcaea9466cfcdda7e0309a3
judith pizzolato <judith pizzolato>
2016-05-19 14:46:46 -0400

    updated the html page and gameCode.js files

---
+++ assets/js/gameCode.js
@@ -0,0 +1,298 @@
+window.onload = function(){
+    var alphabet = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'
+    ];
+    var categories;         // Array of topics
+    var chosenCategory;     // Selected catagory
+    var getHint;            // Word getHint
+    var word;               // Selected word
+    var guess;              // Guess
+    var guesses = [];       // Stored geusses
+    var lives;              // Lives
+    var counter;            // Count correct guesses
+    var space;              // Number of spaces in word '-'
+    var score;               // keeps track of number of score
+
+    // Get elements
+    var showLives = document.getElementById('mylives');
+    var showCatagory = document.getElementById('catagory');
+    var getHint = document.getElementById('hint');
+    var showClue = document.getElementById('clue');
+    var scoreElement = document.getElementById('myScore');
+
+
+    // Create Alphabet ul
+
+    var buttons = function () {
+        myButtons = document.getElementById('buttons');
+        letters = document.createElement('ul');
+
+        for (var i = 0; i < alphabet.length; i++) {
+            letters.id = 'alphabet';
+            list = document.createElement('li');
+            list.id = 'letter';
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
+    }
+
+    // Select Catagory
+    var selectCat = function () {
+        if (chosenCategory === categories[0]) {
+            catagoryName.innerHTML = 'The Chosen Category Is World Wide Soccer Teams';
+        } else if (chosenCategory === categories[1]) {
+            catagoryName.innerHTML = 'The Chosen Category Is Soccer Players';
+        } else if (chosenCategory === categories[2]) {
+            catagoryName.innerHTML = 'The Chosen Category Is World Cup Winning Teams';
+        }
+    }
+
+    // Create guesses ul
+    result = function () {
+        wordHolder = document.getElementById('hold');
+        correct = document.createElement('ul');
+
+        for (var i = 0; i < word.length; i++) {
+            correct.setAttribute('id', 'my-word');
+            guess = document.createElement('li');
+            guess.setAttribute('class', 'guess');
+            if (word[i] === '-') {
+                guess.innerHTML = '-';
+                space = 1;
+            } else {
+                guess.innerHTML = '_';
+            }
+            guesses.push(guess);
+            wordHolder.appendChild(correct);
+            correct.appendChild(guess);
+        }
+    }
+
+    // Show lives
+    comments = function () {
+        showLives.innerHTML = 'You have ' + lives + ' lives';
+        if (lives < 1) {
+            showLives.innerHTML = 'Game Over';
+        }
+        for (var i = 0; i < guesses.length; i++) {
+            if (counter + space === guesses.length) {
+                showLives.innerHTML = 'You Win!';
+            }
+        }
+    }
+
+    // Show Wins
+    var score=0;
+    function resetScore(){
+        score=0;
+        document.getElementById('myScore').value=score;
+    }    
+
+    // Animate man
+    var animate = function () {
+        var drawMe = lives;
+        drawArray[drawMe]();
+    }
+
+    // Hangman
+    canvas = function () {
+        myStickman = document.getElementById('stickman');
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.strokeStyle = '#fff';
+        context.lineWidth = 2;
+    };
+
+    head = function () {
+        myStickman = document.getElementById('stickman');
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.arc(60, 25, 10, 0, Math.PI * 2, true);
+        context.stroke();
+    }
+    draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
+        context.moveTo($pathFromx, $pathFromy);
+        context.lineTo($pathTox, $pathToy);
+        context.stroke();
+    }
+
+    frame1 = function () {
+        draw(0, 150, 150, 150);
+    };
+    frame2 = function () {
+        draw(10, 0, 10, 600);
+    };
+    frame3 = function () {
+        draw(0, 5, 70, 5);
+    };
+    frame4 = function () {
+        draw(60, 5, 60, 15);
+    };
+    torso = function () {
+        draw(60, 36, 60, 70);
+    };
+    rightArm = function () {
+        draw(60, 46, 100, 50);
+    };
+    leftArm = function () {
+        draw(60, 46, 20, 50);
+    };
+    rightLeg = function () {
+        draw(60, 70, 100, 100);
+    };
+    leftLeg = function () {
+        draw(60, 70, 20, 100);
+    };
+    drawArray = [
+        rightLeg,
+        leftLeg,
+        rightArm,
+        leftArm,
+        torso,
+        head,
+        frame4,
+        frame3,
+        frame2,
+        frame1
+    ];
+
+    // OnClick Function
+    check = function () {
+        list.onclick = function () {
+            var guess = this.innerHTML;
+            this.setAttribute('class', 'active');
+            this.onclick = null;
+            for (var i = 0; i < word.length; i++) {
+                //if (window.CP.shouldStopExecution(4)) {
+                    //break;
+                //}
+                if (word[i] === guess) {
+                    guesses[i].innerHTML = guess;
+                    counter += 1;
+                }
+            }
+            //window.CP.exitedLoop(4);
+            var j = (word.indexOf(guess));
+            if (j === -1) {
+                lives -= 1;
+                comments();
+                animate();
+            } else {
+                comments();
+            }
+        }
+    }
+
+    // Play
+    play = function () {
+        categories = [
+            [
+                'real madrid',
+                'paris saint-germain fc',
+                'bayern munchen',
+                'manchester united',
+                'juventus',
+                'corintians',
+                'sporting'
+            ],
+            [
+                'ronaldo',
+                'messi',
+                'neymar',
+                'bale',
+                'suarez'
+            ],
+            [
+                'germany',
+                'spain',
+                'italy',
+                'brazil',
+                'france'
+            ]
+        ];
+        chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+        word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+        word = word.replace(/\s/g, '-');
+        console.log(word);
+        buttons();
+
+        guesses = [];
+        lives = 10;
+        counter = 0;
+        space = 0;
+        result();
+        comments();
+        selectCat();
+        canvas();
+    };
+    play();
+
+    // Hint
+    hint.onclick = function () {
+        hints = [
+            [
+                'Club Team Spain',
+                '2014-2015 French Champions',
+                'German Domestic Team',
+                'Premier League',
+                'Italian League Team',
+                'Brazilian Domestic League',
+                'Portugues Sports Club'
+            ],
+            [
+                'Portuguese player in La Liga',
+                'From Argentina plays for Barcelona',
+                'Plays on Brazil National Team',
+                'From Welch plays for Real Madrid',
+                'Known as Dracula - Messi team mate'
+            ],
+            [
+                'Won 2014',
+                'Won 2010',
+                'Won 2006',
+                'Won 2002',
+                'Won 1998'
+            ]
+        ];
+
+        var catagoryIndex = categories.indexOf(chosenCategory);
+        var hintIndex = chosenCategory.indexOf(word);
+        showClue.innerHTML = 'Clue: - ' + hints[catagoryIndex][hintIndex];
+    };
+
+    // Reset
+    document.getElementById('reset').onclick = function () {
+        correct.parentNode.removeChild(correct);
+        letters.parentNode.removeChild(letters);
+        showClue.innerHTML = '';
+        context.clearRect(0, 0, 400, 400);
+        play();
+    }
+}
